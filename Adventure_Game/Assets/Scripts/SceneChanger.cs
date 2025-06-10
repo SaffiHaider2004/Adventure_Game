94ce87c1b2e565ffe17f7b0359fc385500e4d636
SaffiHaider2004 <saffihaider2004@gmail.com>
2025-06-10 20:55:59 +0500

    assets

---
+++ Adventure_Game/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public void EnterGame()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void EnterName()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void GamePlay()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+}
