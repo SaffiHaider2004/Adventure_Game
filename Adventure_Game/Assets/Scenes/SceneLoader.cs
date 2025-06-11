6762062692457297c9b1b79233e984b092bd04ed
Faizi-ctrl <faizanali95466@gmail.com>
2025-06-11 16:33:26 +0500

    100% Working

--- Adventure_Game/Assets/Scenes/SceneLoader.cs
+++ Adventure_Game/Assets/Scenes/SceneLoader.cs
@@ -5,6 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public void StartingScene()
+    {
+        SceneManager.LoadScene("Starting_Scene"); // Replace with your actual scene name
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("Name_Scene"); // Replace with your actual scene name
+    }
+    public void SettingScene()
+    {
+        SceneManager.LoadScene(1); // Replace with your actual scene name
+    }
+    public void SelectionScene()
+    {
+        SceneManager.LoadScene("CharacterSelection"); // Replace with your actual scene name
+    }
     public void LoadScene1()
     {
         SceneManager.LoadScene("Scene1"); // Replace with your actual scene name
--- Adventure_Game/Assets/Scripts/PlayerStats.cs
+++ Adventure_Game/Assets/Scripts/PlayerStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -115,6 +116,7 @@ public class PlayerStats : MonoBehaviour
     private void Die()
     {
         Debug.Log("Player Died");
+        SceneManager.LoadScene("End_Scene");
         // Handle death (UI, restart, etc.)
     }
     public void StartSprinting()
